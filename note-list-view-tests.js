343804c3520d7c9c37b6ab1f0572e6eac695608d
Henry Cooksley <henry.cooksley@gmail.com>
2018-11-12 16:50:08 +0000

    add http-server

--- note-list-view-tests.js
+++ note-list-view-tests.js
@@ -1,4 +1,4 @@
-function testNoteListView(noteList) {
+function testNoteListViewWithTwoNotes(noteList) {
   var noteListView = new NoteListView(noteList);
   var text1 = noteList.getNotes()[0].text;
   var text2 = noteList.getNotes()[1].text;
@@ -8,4 +8,22 @@ function testNoteListView(noteList) {
 var noteList = new NoteList();
 noteList.createNote("hello");
 noteList.createNote("goodbye");
-testNoteListView(noteList);
+testNoteListViewWithTwoNotes(noteList);
+
+function testNoteListViewWithNoNotes(noteList) {
+  var noteListView = new NoteListView(noteList);
+  assert.isTrue(noteListView.getView() === "<ul></ul>");
+}
+
+var noteList = new NoteList();
+testNoteListViewWithNoNotes(noteList);
+
+function testNoteListViewWithOneNote(noteList) {
+  var noteListView = new NoteListView(noteList);
+  var text1 = noteList.getNotes()[0].text;
+  assert.isTrue(noteListView.getView() === "<ul><li><div>" + text1 + "</div></li></ul>");
+}
+
+var noteList = new NoteList();
+noteList.createNote("hello");
+testNoteListViewWithOneNote(noteList);
