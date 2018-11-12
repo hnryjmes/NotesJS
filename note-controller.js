cd1a70bbbff9bfd1ad984b8c067b38dbe8751d20
Henry Cooksley <henry.cooksley@gmail.com>
2018-11-12 17:36:34 +0000

    halfway through step 6

--- note-controller.js
+++ note-controller.js
@@ -1,1 +1,19 @@
-document.getElementById("app").innerHTML = "howdy";
+//TODO: delete and rewrite using TDD approach
+
+(function(exports) {
+  function NoteController(noteList) {
+    this.noteList = noteList;
+    noteList.createNote("Favourite drink: seltzer");
+    this.noteListView = new NoteListView(noteList);
+  }
+
+  NoteController.prototype.render = function() {
+    document.getElementById('app').innerHTML = this.noteListView.getView();
+  };
+
+  exports.NoteController = NoteController;
+})(this);
+
+var noteList = new NoteList();
+var noteController = new NoteController(noteList);
+noteController.render();
